0c3b6094289e7279150397e8a798d19fa5f37043
sai karthik <saifedev@gmail.com>
2024-07-09 06:20:11 +0000

    added index html code

--- src/main/java/com/example/genAiBot/controller/ChatbotController.java
+++ src/main/java/com/example/genAiBot/controller/ChatbotController.java
@@ -1,5 +1,16 @@
 package com.example.genAiBot.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 public class ChatbotController {
     
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+  
 }
