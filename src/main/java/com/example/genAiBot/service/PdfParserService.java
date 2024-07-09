4ab732b631aba73c4e22b83cc4c83248e3d9c996
sai karthik <saifedev@gmail.com>
2024-07-09 06:01:28 +0000

    add pdfbox dependency

--- src/main/java/com/example/genAiBot/service/PdfParserService.java
+++ src/main/java/com/example/genAiBot/service/PdfParserService.java
@@ -1,5 +1,19 @@
 package com.example.genAiBot.service;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
 public class PdfParserService {
+
+    public String parsePDF(File file) throws IOException {
+            PDDocument document = PDDocument.load(file);
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+            document.close();
+            return text;
+    }
     
 }
