5a52fb2cda434f8884f5b3e931a975d947b70bde
saife237 <saifedev@gmail.com>
2024-07-09 10:56:32 +0530

    first commit

---
+++ src/main/java/com/example/genAiBot/GenAiBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.genAiBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/genAiBot/GenAiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genAiBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenAiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
